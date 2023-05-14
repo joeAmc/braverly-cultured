d4d7a455206aabbd5d0f07a988fd761d85d1f21e
joeAmc <joe@limio.com>
2023-05-14 22:20:54 +0100

    initial set up

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = () => {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  return <div>Nav</div>;
+};
+
+export default Nav;
