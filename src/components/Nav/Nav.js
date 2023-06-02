94670acd823088aabe2b4daa540ef6636f975f45
joeAmc <joe@limio.com>
2023-06-02 21:32:28 +0100

    nav and footer updates

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,80 @@
 import React from "react";
-import "./Nav.css";
+import { useEffect } from "react";
+import "../Nav/Nav.css";
 
-const Nav = () => {
+import { Link, useLocation } from "react-router-dom";
+import { BsThreeDots } from "react-icons/bs";
+
+const Header = () => {
+  const location = useLocation();
+
   const scrollTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   };
 
-  return <div>Nav</div>;
+  // useEffect(() => {
+  //   const divHeight = document.querySelector(".banner-img").clientHeight;
+
+  //   const navStart = () => {
+  //     if (location.pathname === "/" && window.scrollY > divHeight) {
+  //       setHeader(true);
+  //     } else if (location.pathname !== "/") {
+  //       setHeader(true);
+  //     } else {
+  //       setHeader(false);
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", navStart);
+  //   return () => window.EventListener("scroll", navStart);
+  // }, []);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", isSticky);
+  //   return () => {
+  //     window.removeEventListener("scroll", isSticky);
+  //   };
+  // });
+
+  // const isSticky = () => {
+  //   const nav = document.querySelector(".nav");
+  //   const scrollTop = window.scrollY;
+  //   if (location.pathname === "/") {
+  //     scrollTop >= window.innerHeight
+  //       ? nav.classList.add("active")
+  //       : nav.classList.remove("active");
+  //   } else nav.classList.add("active");
+  // };
+
+  return (
+    // <div className="header">
+    <div className="nav">
+      {/* <div className="nav-text"> */}
+      <h1>Braverly Cultured</h1>
+
+      <div className="links">
+        <div className="about-link">
+          <a href="#about">
+            <h3>About</h3>
+            <BsThreeDots />
+          </a>
+        </div>
+        <div className="collect-link">
+          <a href="#team">
+            <h3>Team</h3>
+            <BsThreeDots />
+          </a>
+        </div>
+        <div className="contact-link">
+          <a href="#contact">
+            <h3>Contact</h3>
+            <BsThreeDots />
+          </a>
+        </div>
+      </div>
+      {/* </div> */}
+    </div>
+  );
 };
 
-export default Nav;
+export default Header;
