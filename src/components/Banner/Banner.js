5d994eae4e243b19d2811658bd40ffb03d0002c1
joeAmc <joe@limio.com>
2023-06-03 12:20:54 +0100

    nav and banner updates

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,9 +5,14 @@ const Banner = () => {
   return (
     <section>
       <div className="banner-container">
+        <div className="overlay"></div>
         {/* <img className="banner-img" src={banner} alt="" /> */}
         <div className="banner-content">
-          <h1>Midas Touch Jewellery</h1>
+          <h1>
+            Natures microbiome has the answers to today’s food production
+            problems
+          </h1>
+          <h2>We help to find them</h2>
         </div>
       </div>
     </section>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,77 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Nav/Nav.css";
 import { HashLink as Link } from "react-router-hash-link";
 import Logo from "../../assets/logo-white.png";
+import { FiMenu } from "react-icons/fi";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const scrollTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   };
 
-  // useEffect(() => {
-  //   const divHeight = document.querySelector(".banner-img").clientHeight;
-
-  //   const navStart = () => {
-  //     if (location.pathname === "/" && window.scrollY > divHeight) {
-  //       setHeader(true);
-  //     } else if (location.pathname !== "/") {
-  //       setHeader(true);
-  //     } else {
-  //       setHeader(false);
-  //     }
-  //   };
-
-  //   window.addEventListener("scroll", navStart);
-  //   return () => window.EventListener("scroll", navStart);
-  // }, []);
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", isSticky);
-  //   return () => {
-  //     window.removeEventListener("scroll", isSticky);
-  //   };
-  // });
-
-  // const isSticky = () => {
-  //   const nav = document.querySelector(".nav");
-  //   const scrollTop = window.scrollY;
-  //   if (location.pathname === "/") {
-  //     scrollTop >= window.innerHeight
-  //       ? nav.classList.add("active")
-  //       : nav.classList.remove("active");
-  //   } else nav.classList.add("active");
-  // };
-
   return (
-    // <div className="header">
     <div className="nav">
-      {/* <div className="nav-text"> */}
-      <div>
+      <div className="nav-logo">
         <img onClick={scrollTop} src={Logo} alt="logo" />
       </div>
-      <div className="links">
+      <div className={`links${menuOpen ? " open" : ""}`}>
         <div className="home-link">
           <Link to="/" onClick={scrollTop}>
-            <h3>HOME</h3>
+            <h3>home</h3>
           </Link>
         </div>
         <div className="about-link">
           <a href="#about">
-            <h3>ABOUT US</h3>
+            <h3>about us</h3>
           </a>
         </div>
         <div className="collect-link">
           <a href="#team">
-            <h3>TEAM</h3>
+            <h3>team</h3>
           </a>
         </div>
         <div className="contact-link">
           <a href="#contact">
-            <h3>OUR WAY?</h3>
+            <h3>our why?</h3>
           </a>
         </div>
       </div>
-      {/* </div> */}
+      <FiMenu
+        className={`menu-icon${menuOpen ? " open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      />
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   );
 };
